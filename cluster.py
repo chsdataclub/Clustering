5a06e8592491e759cc816bf4af0590e036638d08
J Stigter <jaredstigter@gmail.com>
2018-03-04 15:37:02 -0500

    eliminated the default did all the graphing

--- cluster.py
+++ cluster.py
@@ -6,38 +6,60 @@ import sklearn.cluster as cluster
 import time
 import random
 
-def plot_clusters(x, y, labelx, labely, data, algorithm, args, kwds):
-    X = np.array([[4,0],
-                  [0,4],
-                  [2,2]],np.float64)
-    a = algorithm(init=X, n_init=1, *args, **kwds)
-    print(a.init)
-    passin= [[data[x][a],data[y][a]] for a in range(0, len(data[x]))]
+
+def plot_clusters(z, za, labelx, labely, badx, bady, algorithm, kwds):
+    a = algorithm(**kwds)
+    print('start1')
+    x = z.tolist()
+    y = za.tolist()
+    print('x', [i for i in range(0, len(x)) if x[i] == badx])
+    print('y', [i for i in range(0, len(y)) if y[i] == bady])
+    inc = 0
+    while inc < len(x):
+        if x[inc] == badx or y[inc] == bady or x[inc] == str(badx) or y[inc] == str(bady) or x[inc] == ' ' or y[inc] == ' ':
+            del x[inc]
+            del y[inc]
+            # x.pop(inc)
+            # y.pop(i)
+            inc -= 1
+
+        inc += 1
+    print('start2')
+    print('x', [i for i in range(0, len(x)) if x[i] == badx])
+    print('y', [i for i in range(0, len(y)) if y[i] == bady])
+    passin = [[x[i], y[i]] for i in range(0, len(x))]
     labels = a.fit_predict(passin)
+    print('start3')
     palette = sns.color_palette('pastel', np.unique(labels).max() + 1)
     colors = [palette[x] if x >= 0 else (0.0, 0.0, 0.0) for x in labels]
-    plt.scatter(data[x], data[y], c=colors, **plot_kwds)
-    plt.scatter(a.cluster_centers_[:, 0], a.cluster_centers_[:, 1], marker="x", **plot_kwds)
+    print('x', [i for i in range(0, len(x)) if x[i] == badx])
+    print('y', [i for i in range(0, len(y)) if y[i] == bady])
+    plt.scatter(x, y, c=colors, **plot_kwds)
+    plt.scatter(a.cluster_centers_[:,
+                0], a.cluster_centers_[:, 1], c=palette, marker="x",
+                **{'alpha': 1, 's': 80, 'linewidths': 0})
     frame = plt.gca()
     frame.axes.get_xaxis().set_visible(True)
     frame.set_xlabel(labelx)
     frame.axes.get_yaxis().set_visible(True)
     frame.set_ylabel(labely)
-    plt.title('Clusters found by {}'.format(str(algorithm.__name__)), fontsize=24)
+    plt.title(labely + ' vs ' + labelx, fontsize=24)
     plt.show()
+
 
 sns.set_context('poster')
 sns.set_color_codes()
-plot_kwds = {'alpha': 1, 's': 80, 'linewidths': 0} #alpha .01
+plot_kwds = {'alpha': .01, 's': 80, 'linewidths': 0}  # alpha .01
 
-#reads all the data from Pew Research
+# reads all the data from Pew Research
 f = open('data.csv')
 data = pd.DataFrame(pd.read_csv(f))
 f.close()
 
-#plt.scatter(data['q5'], data['q9'], c='b', **plot_kwds)
-frame = plt.gca()
-frame.axes.get_xaxis().set_visible(False)
-frame.axes.get_yaxis().set_visible(False)
+input = [data['income'], data['q40jf1'], 'income', 'Immigration importance', 10, 9]
+print('start')
+plot_clusters(*input, cluster.KMeans, {'n_clusters': 4})
 
-plot_clusters('q40e', 'q40f', 'Terrorism', 'Supreme court appiontments', data, cluster.KMeans, (), {'n_clusters': 3})
+plot_clusters(*input, cluster.KMeans, {'n_clusters': 3})
+
+plot_clusters(*input, cluster.KMeans, {'n_clusters': 2})
